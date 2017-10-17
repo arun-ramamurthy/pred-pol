21c9188d136c4cd183f9671c20c0526c17b6d999
Vaibhav Ramamoorthy <vaibhavr@berkeley.edu>
2017-10-17 11:34:33 -0700

    Vaibhav/

---
+++ Vaibhav/exploration.R
@@ -0,0 +1,3 @@
+oak <- read.csv("/Users/vaibhav/Documents/Year4_Senior/Semester 1/stat157/predictive-policing/01_import/input/drug_crimes_with_bins.csv")
+oak_grid <- readRDS("/Users/vaibhav/Documents/Year4_Senior/Semester 1/stat157/predictive-policing/01_import/input/oakland_grid_data.rds")
+oak_outline <- readRDS("/Users/vaibhav/Documents/Year4_Senior/Semester 1/stat157/predictive-policing/01_import/input/oakland_outline.rds")
