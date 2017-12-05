16c082d2d9dbca1b7788b93a12b54f28a88adeef
Evan Limanto <evanlimanto@berkeley.edu>
2017-12-05 00:18:14 -0800

    Initial set of tests

--- analyses/model.R
+++ analyses/model.R
@@ -23,7 +23,7 @@ oak_agg <- oak %>%
   summarize(num_crimes = n(), mean_lag = mean(LAG)) %>%
   ungroup()
 
-get_trailing_table <- function(df, date, n) {
+get_trailing_table <- function(df, today, n) {
   # Takes in a dataframe containing date/num_crimes data and returns
   # another dataframe containing the total number of crimes over the last
   # n days before date, per bin.
@@ -35,9 +35,8 @@ get_trailing_table <- function(df, date, n) {
   #
   # Returns:
   #   A table containing filtered dates from previous n days
-  date <- as.Date(date)
-  daterange <- seq(from = date - n, length.out = n, by = 1)
-  df <- df %>% filter(date %in% daterange)
+  today <- as.Date(today)
+  df <- df %>% subset((date >= (today - n)) & (date < today))
 
   return(df)
 }
@@ -90,30 +89,36 @@ get_bin_scores <- function(df, date, r) {
 }
 
 get_predicted_bins_helper <- function(bin, bin_scores, s) {
+  # Function to be sapplied to a bin to compute its score
+  # considering the weighted score of its neighbors.
   neighbors <- touching_dict[[bin]]
   final <- s*sum(bin_scores$bin_score[which(bin_scores$bin %in% neighbors)]) +
     (1-s)*bin_scores$bin_score[which(bin_scores$bin == bin)]
   return(final)
 }
 
-# Get predicted bins for deployment of K police on DATE using
-# data from N days trailing using R rate of discounting,
-# and scaling neighbor bin scores by S
 get_predicted_bins <- function(df, date, k, n, r, s) {
+  # Get bins to deploy police on.
+  #
+  # Args:
+  #   df: Data frame containing data points.
+  #   date: Date to compute bins for "YYYY-MM-DD".
+  #   k: Number of police to deploy.
+  #   n: Number of trailing days to consider.
+  #   r: Rate of discounting to use.
+  #   s: Scaling constant applied to scores of neighbor bins.
   date <- as.Date(date)
   t <- get_trailing_table(df, date, n)
   bin_scores <- get_bin_scores(t, date, r)
   bins <- bin_scores$bin
   final_score <- sapply(bins, get_predicted_bins_helper, bin_scores, s)
-  new_bin_scores <- data.frame(bins, final_score)
-  new_bin_scores <- new_bin_scores %>%
-    arrange(desc(final_score))
+  new_bin_scores <- data.frame(bins, final_score) %>% arrange(desc(final_score))
   return(new_bin_scores$bins[1:k])
 }
 
-# Gets the best capture rate achievable on TODAY given
-# K deployments using DF of crime totals
 get_maximal_capture <- function(df, today, k) {
+  # Gets the maximum capture rate achievable for a certain date
+  # and number of deployments.
   df <- df %>%
     filter(date == today) %>%
     arrange(desc(num_crimes))
@@ -199,33 +204,35 @@ get_average_predpol_capture_rate <- function(df, k, lum_data, date_samp) {
 ### PARAMETER TUNING ###
 ########################
 
-params <- list(r = seq(from = 0, to = 0.2, by = 0.02),
-               s = seq(from = 0, to = 0.5, by = 0.05)) %>%
-  cross_df()
-
-set.seed(157)
-sampDates <- base::sample(seq(as.Date("2010-12-28"), as.Date("2011-12-30"), by = 1), size = 50)
-mod <- function(r, s) {
-  get_average_achieved_capture_rate(sampDates, oak_agg, 20, 365, r, s)
+run = function() {
+  params <- list(r = seq(from = 0, to = 0.2, by = 0.02),
+                 s = seq(from = 0, to = 0.5, by = 0.05)) %>%
+    cross_df()
+  
+  set.seed(157)
+  sampDates <- base::sample(seq(as.Date("2010-12-28"), as.Date("2011-12-30"), by = 1), size = 50)
+  mod <- function(r, s) {
+    get_average_achieved_capture_rate(sampDates, oak_agg, 20, 365, r, s)
+  }
+  
+  # params <- params %>% mutate(capture_rate = pmap(params, mod))
+  
+  # capture_rate <- mapply(mod, params$r, params$s)
+  
+  library(parallel)
+  
+  # Number of cores
+  nCores <- detectCores() - 1
+  
+  # Creating cluster
+  clust <- makeCluster(nCores)
+  
+  # create matrix of parameters
+  p <- as.matrix(params)
+  
+  # exporting functions to cluster
+  clusterExport(clust, list("get_average_achieved_capture_rate"))
+  
+  # Parallelized apply
+  capture_rate <- parRapply(clust, p, mod)
 }
-
-# params <- params %>% mutate(capture_rate = pmap(params, mod))
-
-# capture_rate <- mapply(mod, params$r, params$s)
-
-library(parallel)
-
-# Number of cores
-nCores <- detectCores() - 1
-
-# Creating cluster
-clust <- makeCluster(nCores)
-
-# create matrix of parameters
-p <- as.matrix(params)
-
-# exporting functions to cluster
-clusterExport(clust, list("get_average_achieved_capture_rate"))
-
-# Parallelized apply
-capture_rate <- parRapply(clust, p, mod)
