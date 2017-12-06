106ff51f42a3800e92410fd3dc32ab70c62d1e54
Jong Lee <jonghalee@berkeley.edu>
2017-12-06 08:47:42 -0800

    AUC initial stuff

--- analyses/auc.R
+++ analyses/auc.R
@@ -2,20 +2,40 @@
 ###############
 ### auc.R #####
 ###############
-
+setwd("~/Desktop/School/STAT 157/predictive-policing")
 params <- readRDS("analyses/parameter_tuning.rds")
 lum <- readRDS("analyses/lum_testing_rates.rds")
 
 results50 <- readRDS("analyses/results50.rds")
-View(results50$results)
 
 ourRates <- c()
 lumRates <- c()
 
 source("analyses/model.R")
 
-useDf <- subset(oak_agg, date > min(oak_agg$date) + 365 )
-for (k in 10:30) {
-  
-  get_average_achieved_capture_rate(unique(oak_agg$date))
+# Predpol only has data >= 2010-12-28 dates, and has some dates missing
+predpolDates <- gsub("X", "", colnames(predpol_preds))[-1]
+oakaggDates <- format(as.Date(unique(oak_agg$date)),  format="%Y.%m.%d")
+toUseDates <- oakaggDates[oakaggDates %in% predpolDates]
+toUseDates <- as.Date(gsub("\\.", "-", toUseDates))
+useDf <- subset(oak_agg, date %in% toUseDates)
+
+for (i in 10:30) {
+  ourRates <- c(ourRates,
+                get_average_achieved_capture_rate(
+                  date_samp = unique(useDf$date), df = oak_agg, 
+                  touching_dict = touching_dict, 
+                  k = i, n = 365, r = 0, s = 0)
+                )
+  lumRates <- c(lumRates, 
+                get_average_predpol_capture_rate(
+                  df = oak_agg, date_samp = unique(useDf$date), 
+                  k = i, lum_data = predpol_preds))
+  cat("Finished running for k = ", i, "...\n")
 }
+names(ourRates) <- c(10:30)
+names(lumRates) <- c(10:30)
+
+aucData <- list(ourRates, lumRates)
+saveRDS(aucData, file = "analyses/aucData.rds")
+
